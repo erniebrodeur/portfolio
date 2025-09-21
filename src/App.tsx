46a0e6b1fa165ca38c31f21771c64efaf0e8a5da
Ernie Brodeur <ebrodeur@ujami.net>
2025-09-21 11:05:01 -0700

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import Header from './components/Header'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  )
+export default function App() {
+    return (
+        <div className="min-h-screen bg-gray-900 text-white">
+            <Header />
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+
+            <footer className="py-8 text-center text-gray-400">
+                <p>&copy; 2025 Ernie Brodeur. All rights reserved.</p>
+            </footer>
+        </div>
+    );
 }
-
-export default App
